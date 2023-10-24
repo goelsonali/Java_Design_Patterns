4a354f69677e5de0aa9f79d25b22198404794254
Sonali Goel <Sonali.Goel@ynap.com>
2023-10-24 14:14:00 +0100

    Correcting the FactoryMethod design pattern

--- src/main/java/com/design/patterns/creational/factoryMethod/NonVegBurger.java
+++ src/main/java/com/design/patterns/creational/factoryMethod/NonVegBurger.java
@@ -1,13 +1,9 @@
 package com.design.patterns.creational.factoryMethod;
 
-public class NonVegBurger extends Restaurant implements Burger{
+public class NonVegBurger implements Burger{
     @Override
     public void prepare() {
         System.out.println("Preparing Non-Veg Burger");
     }
 
-    @Override
-    public Burger createBurger() {
-        return new NonVegBurger();
-    }
 }
